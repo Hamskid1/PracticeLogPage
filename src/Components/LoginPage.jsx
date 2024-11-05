c91466bbcfd6ff5109411a0956bb78b997d3c734
Hamksid <hameedlateef343@gmail.com>
2024-11-05 06:12:09 +0100

    creating login componenets

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { FaLock, FaUser } from 'react-icons/fa';
+import './LoginPage.css';
 
 function LoginPage() {
   return (
-    <div>LoginPage</div>
-  )
+    <div className='containers'> 
+      <h1>Login</h1>
+      <form action="#">
+        <div className='input-type'>
+          <FaUser className='i' />
+          <input type="text" placeholder='Username' className='input' />
+        </div>
+
+        <div className='input-pass'>
+          <FaLock className='i2'/>
+          <input type="password" placeholder='Password' className='input' />
+        </div>
+
+        <div className="remember-forget">
+          <label><input type="checkbox" /> Remember</label>
+          <a href="#">Forget Password</a>
+        </div>
+
+        <button type="submit" className='login-btn'>Login</button>
+
+        <div className="register-link">
+          <p>Don't have an account? <a href="#">Register</a></p>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;

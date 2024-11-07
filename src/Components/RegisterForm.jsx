c7f1d2ca0cd5aed0dd2fe3d80a84ecb039133c3f
Hamksid <hameedlateef343@gmail.com>
2024-11-07 06:31:54 +0100

    creating register components and editing older components

--- src/Components/RegisterForm.jsx
+++ src/Components/RegisterForm.jsx
@@ -1,32 +1,32 @@
-// LoginForm.js
+// RegisterForm.js
 import React from 'react';
 import { FaLock, FaUser } from 'react-icons/fa';
 
-function LoginPage({ onToggleForm }) {
+
+function RegisterForm({ onToggleForm }) {
   return (
     <div>
-      <h1>Login</h1>
+      <h1>Register</h1>
       <form action="#">
         <div className='input-type'>
           <FaUser className='i' />
           <input type="text" placeholder='Username' className='input' />
         </div>
+        <div className='input-type'>
+          <input type="email" placeholder='Email' className='input' />
+        </div>
         <div className='input-pass'>
           <FaLock className='i2' />
           <input type="password" placeholder='Password' className='input' />
         </div>
-        <div className="remember-forget">
-          <label><input type="checkbox" /> Remember</label>
-          <a href="#">Forget Password</a>
-        </div>
-        <button type="submit" className='login-btn'>Login</button>
+        <button type="submit" className='login-btn'>Register</button>
       </form>
 
       <div className="register-link">
-        <p>Don't have an account? <a href="#" onClick={onToggleForm}>Register</a></p>
+        <p>Already have an account? <a href="#" onClick={onToggleForm}>Login</a></p>
       </div>
     </div>
   );
 }
 
-export default LoginPage;
+export default RegisterForm;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 import LoginPage from "../Components/LoginPage";
+import RegisterForm from "../Components/RegisterForm";
+import "../Components/LoginPage.css"
 
 const HomePage = () => {
-  return (
-    <div>
-      <LoginPage/>
-    </div>
-  );
+    // State to control form display
+    const [isRegistering, setIsRegistering] = useState(false);
+  
+    // Toggle form display
+    const toggleForm = () => setIsRegistering((prev) => !prev);
+  
+    return (
+      <div className='containers'>
+        {isRegistering ? (
+          <RegisterForm onToggleForm={toggleForm} />
+        ) : (
+          <LoginPage onToggleForm={toggleForm} />
+        )}
+      </div>
+    );
 };
 
 export default HomePage;
